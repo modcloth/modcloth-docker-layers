5c0fa085dc1f44a2462dfe20086016fcaf58f4d9
Rafe Colton <r.colton@modcloth.com>
2013-12-12 14:18:00 -0800

    AGAIN trying to fix make clean

--- meta.mk
+++ meta.mk
@@ -32,8 +32,9 @@ delete_current_tag:
 
 clean:
 	rm -f .latest_container .latest_tagged .latest_pushed .container_output
-	$(DOCKER) images | grep $(REGISTRY)/$(PROJECT) | awk '{ print $$3 }' | \
-	  sort | uniq | xargs -I {} test -z "{}" || $(DOCKER) rmi {}
+	export IMAGES="$(shell $(DOCKER) images | grep $(REGISTRY)/$(PROJECTS))" ; \
+		test -z "$$IMAGES" || echo $$IMAGES | awk '{ print $$3 }' | \
+		sort | uniq | xargs $(DOCKER) rmi
 
 .latest_container: .pull
 	rm -f $@
